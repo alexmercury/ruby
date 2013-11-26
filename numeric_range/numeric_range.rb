e953438ce135b05f9883cfe8bd4ab90b001856a4
alexsummers <alexsummers@yandex.ua>
2013-11-26 12:11:18 +0200

    add new

--- numeric_range/numeric_range.rb
+++ numeric_range/numeric_range.rb
@@ -1,16 +1,17 @@
-def num(times)
+def numeric_range(count)
   text = '1'
   arr = []
-  times.to_i.times do
+  count.times do
     str = ''
     char = '0'
+    puts text
     text.each_char do |c|
       if c == char
         arr[0] += 1
       else
         str += arr.join('').to_s
         arr.clear
-        arr = [1,c]
+        arr = [1, c]
       end
       char = c
     end
@@ -21,4 +22,4 @@ def num(times)
   text
 end
 
-puts num(40).length
+puts numeric_range(3).length
